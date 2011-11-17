4126b94926f2e5739e076c6ab9b8e382e5d14704
Micha Gorelick <mynameisfiber@gmail.com>
2011-11-17 14:56:34 -0500

    Added zsh config and updated setup script

--- setup.bash
+++ setup.bash
@@ -12,5 +12,15 @@ if [[ ! `grep ". $CONFIGPATH/bash_custom" ~/.bashrc` ]]; then
     echo -ne "if [ -f $CONFIGPATH/bash_custom ]; then\n  . $CONFIGPATH/bash_custom \nfi" >> ~/.bashrc
 fi
 
+echo "Configuring ZSH"
+ln -sf $CONFIGPATH/oh-my-zsh ~/.oh-my-zsh
+ln -sf $CONFIGPATH/zshrc ~/.zshrc
+
 echo "Configuring SCREEN"
 ln -sf $CONFIGPATH/screenrc ~/.screenrc
+
+echo "Installing VIM plugins"
+git submodule update --init
+
+echo "Upgrading VIM plugins (this may be redundant)"
+git submodule foreach git pull origin master
