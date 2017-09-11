07a544e7732db9da65bb059e4f509551b11d979f
Micha Gorelick <mynameisfiber@gmail.com>
2017-09-11 14:31:18 -0400

    change to zenburn awesome theme... i should probably move that into this git repo?

--- dotfiles/.config/awesome/rc.lua
+++ dotfiles/.config/awesome/rc.lua
@@ -44,7 +44,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(awful.util.get_themes_dir() .. "default/theme.lua")
+beautiful.init(awful.util.get_themes_dir() .. "zenburn/theme.lua")
 beautiful.wallpaper = awful.util.get_configuration_dir() .. "/wallpaper.jpg"
 
 -- This is used later as the default terminal and editor to run.
