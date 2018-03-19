7242aed4d908506221d8a2673c4327480134e893
Micha Gorelick <mynameisfiber@gmail.com>
2018-03-19 19:39:02 -0400

    layouts, ping script, vim bundles

---
+++ dotfiles/.screenlayouts/gakken.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL2 --off --output VIRTUAL1 --off --output eDP1 --primary --mode 1366x768 --pos 640x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --mode 640x480 --pos 0x0 --rotate normal --output DP2 --off
---
+++ dotfiles/.screenlayouts/work.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL2 --off --output VIRTUAL1 --off --output eDP1 --primary --mode 1366x768 --pos 0x200 --rotate normal --output DP1 --mode 1920x1200 --pos 1366x0 --rotate left --output HDMI2 --off --output HDMI1 --off --output DP2 --off
