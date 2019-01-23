a2bdcede5784a64777a3f33665553698cb4f8088
Micha Gorelick <mynameisfiber@gmail.com>
2019-01-23 13:14:05 -0500

    Update alacritty and pyenv install/update scripts

--- scripts/install_alacritty.sh
+++ scripts/install_alacritty.sh
@@ -8,6 +8,7 @@ fi
 if ! which rustup > /dev/null; then
     echo "Installing rust"
     curl https://sh.rustup.rs -sSf | sh -s -- -y
+    . $HOME/.bashrc
 else
     echo "Upgrading rust"
     rustup update stable
@@ -27,6 +28,14 @@ mkdir -p $rootdir/deb/
 cd $scratch
 git clone https://github.com/jwilm/alacritty.git
 cd alacritty
+if command -v alacritty > /dev/null 2>&1; then
+    cur_version=$( alacritty --version | cut -f2 -d' ' )
+    new_version=$( grep -E "^version =" Cargo.toml | cut -f2 -d'=' | tr -dc [0-9.] )
+    if [ "$cur_version" == "$new_version" ]; then
+        echo "Latest version already installed!"
+        exit
+    fi
+fi
 cargo build --release
 cargo install cargo-deb
 cargo deb --no-build --install --output $rootdir/deb/
