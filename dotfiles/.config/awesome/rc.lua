bdc91dd9b8c57e24c42bb5165fff879ccbbd898c
Micha Gorelick <mynameisfiber@gmail.com>
2019-01-24 02:09:05 -0500

    more updates for electron

--- dotfiles/.config/awesome/rc.lua
+++ dotfiles/.config/awesome/rc.lua
@@ -58,11 +58,11 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
+    awful.layout.suit.tile,
+    awful.layout.suit.tile.bottom,
     awful.layout.suit.spiral.dwindle,
     awful.layout.suit.floating,
-    awful.layout.suit.tile,
     --awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
     --awful.layout.suit.tile.top,
     --awful.layout.suit.fair,
     --awful.layout.suit.fair.horizontal,
