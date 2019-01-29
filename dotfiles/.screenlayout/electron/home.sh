32b3797439f0caa9848bc1493da2f9885e7bf736
Micha Gorelick <mynameisfiber@gmail.com>
2019-01-29 11:07:05 -0500

    Made screen layouts support multiple hosts more nicely

---
+++ dotfiles/.screenlayout/electron/home.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1280x720 --pos 1920x48 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --off
+
+xmodmap ~/.Xmodmap
