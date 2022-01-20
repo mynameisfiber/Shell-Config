37d2aaa6534c499ba2f21119a812c84e108d7692
Micha Gorelick <mynameisfiber@gmail.com>
2022-01-20 09:45:04 +0100

    update timer script and screenlayouts

--- dotfiles/.screenlayout/charm/code.sh
+++ dotfiles/.screenlayout/charm/code.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP --primary --mode 1920x1080 --pos 0x183 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 1920x0 --rotate left --output DisplayPort-0 --off
+xrandr --output eDP --primary --mode 1920x1080 --pos 0x183 --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --mode 1920x1080 --pos 1920x0 --rotate left
